58f45241de8c45ea5c9e5abeab8c636cc65feb99
marcotf-git <marcotf.dev@gmail.com>
2018-06-29 20:00:25 -0300

    with icons

--- app/src/main/java/com/example/androidstudio/capstoneproject/ui/MainActivity.java
+++ app/src/main/java/com/example/androidstudio/capstoneproject/ui/MainActivity.java
@@ -17,6 +17,7 @@ import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -27,6 +28,9 @@ import android.widget.Toast;
 import com.example.androidstudio.capstoneproject.IdlingResource.SimpleIdlingResource;
 import com.example.androidstudio.capstoneproject.R;
 import com.example.androidstudio.capstoneproject.data.LessonsContract;
+import com.example.androidstudio.capstoneproject.data.TestUtil;
+
+import java.util.Objects;
 
 
 /**
@@ -71,6 +75,7 @@ public class MainActivity extends AppCompatActivity implements
     private long selectedLesson_id;
 
     private Menu mMenu;
+    private Toolbar mToolbar;
 
     // flag for preference updates
     private static boolean flag_preferences_updates = false;
@@ -116,6 +121,10 @@ public class MainActivity extends AppCompatActivity implements
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Add the toolbar as the defaulr app bar
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
 
         mContext = this;
 
@@ -428,6 +437,8 @@ public class MainActivity extends AppCompatActivity implements
         selectedLesson_id = -1;
 
     }
+
+
 
 
 }
