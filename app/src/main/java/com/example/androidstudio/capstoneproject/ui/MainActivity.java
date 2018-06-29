01110a1cb5b577ac57412c3b7fffbd2f73f674d8
marcotf-git <marcotf.dev@gmail.com>
2018-06-29 05:35:51 -0300

    position rotation corrected

--- app/src/main/java/com/example/androidstudio/capstoneproject/ui/MainActivity.java
+++ app/src/main/java/com/example/androidstudio/capstoneproject/ui/MainActivity.java
@@ -5,37 +5,28 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.Parcelable;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.VisibleForTesting;
 import android.support.design.widget.Snackbar;
 import android.support.test.espresso.IdlingResource;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.FrameLayout;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.androidstudio.capstoneproject.IdlingResource.SimpleIdlingResource;
 import com.example.androidstudio.capstoneproject.R;
 import com.example.androidstudio.capstoneproject.data.LessonsContract;
-import com.example.androidstudio.capstoneproject.data.TestUtil;
 
 
 /**
@@ -138,8 +129,7 @@ public class MainActivity extends AppCompatActivity implements
 
         FragmentManager fragmentManager = getSupportFragmentManager();
 
-        // Create fragments if doesn't exist
-        // The data for the fragment will be set up in the loader
+        // Only create fragment when needed
         if (savedInstanceState == null) {
             Log.v(TAG, "creating fragment");
             mainFragment = new MainFragment();
